9f3d12329f31a998a78cd6816d2b2dd28328d63f
bernathmark <nitro_5_bernath_mark@outlook.hu>
2023-02-20 20:37:57 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: '../assets/tree.jpeg',
+      username: 'nature',
+      content: 'Some shit about some shit',
+    },
+    {
+      title: 'Big mountain',
+      imageUrl: '../assets/mountain.jpeg',
+      username: 'fucking fuck',
+      content: 'Some shit about some shit',
+    },
+    {
+      title: 'Bikini',
+      imageUrl: '../assets/biking.jpeg',
+      username: 'gay stuff',
+      content: 'Some shit about some shit',
+    },
+  ];
 }
